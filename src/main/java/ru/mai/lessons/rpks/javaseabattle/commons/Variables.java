ed427173b3794d3328249c979ff57489c04d56ce
nenavision <nekitlomic@gmail.com>
2023-12-11 21:32:23 +0100

    THE "STYLIEST" (REALLY BETTER THAN #1) SEA BATTLE IN THE REPO IS DONE

---
+++ src/main/java/ru/mai/lessons/rpks/javaseabattle/commons/Variables.java
@@ -0,0 +1,13 @@
+package ru.mai.lessons.rpks.javaseabattle.commons;
+
+public class Variables {
+    public static final String TURN = "You shoot";
+    public static final String MISS = "MISS! :(";
+    public static final String GOTHIM = "GOT HIM!";
+    public static final String DEADNOW = "DEAD now";
+    public static final String WINNER = "WIN!!!";
+    public static final String LOSER = "YOU LOST!";
+    public static final String LEAVER = "Leaver!";
+    public static final String OPPONENT_LEFT = "ENEMY left";
+    public static final String OPPONENT_IS_THINKING = "ENEMY's turn";
+}
---
+++ src/main/java/ru/mai/lessons/rpks/javaseabattle/server/Server.java
@@ -0,0 +1,264 @@
+package ru.mai.lessons.rpks.javaseabattle.server;
+
+import org.apache.log4j.Logger;
+import ru.mai.lessons.rpks.javaseabattle.commons.PointsImpl;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+import static ru.mai.lessons.rpks.javaseabattle.commons.Variables.*;
+
+public class Server {
+
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
+
+    private String host = "localhost";
+    private Integer port = 8832;
+    private ClientHandler playerOne;
+    private ClientHandler playerTwo;
+
+    private Map<PointsImpl, Boolean> firstPlayerField;
+    private Map<PointsImpl, Boolean> secondPlayerField;
+
+    public Server() {
+    }
+
+    public Server(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() {
+
+        logger.info("Server initialization");
+        ServerSocket serverSocket = null;
+
+        try {
+
+            serverSocket = new ServerSocket(port);
+            logger.info("Server is waiting for clients");
+
+            while (!canGameStart()) {
+                Socket client = serverSocket.accept();
+                logger.info("New client connected: " + client.toString());
+
+                ClientHandler clientHandler = new ClientHandler(client, this);
+                addPlayer(clientHandler);
+                new Thread(clientHandler).start();
+            }
+
+            logger.info("Game started");
+
+            game();
+
+            logger.info("Game was ended");
+
+        } catch (IOException e) {
+            logger.error("Problem with server", e);
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException ex) {
+                    logger.error("Failed to close server", ex);
+                }
+            }
+        }
+    }
+
+    private void game() {
+
+        ClientHandler turnClient = playerOne;
+        boolean needSwitch = true;
+
+        for (int i = 0; playersOnline() && i < 2; ++i) {
+
+            turnClient.sendMessage(TURN);
+
+            String turn = turnClient.getClientMessage();
+
+            initializeField(turn, turnClient);
+
+            turnClient = switchClient(turnClient);
+        }
+
+        while (playersOnline()) {
+
+            if (needSwitch) {
+                turnClient.sendMessage(TURN);
+            }
+
+            String turn = turnClient.getClientMessage();
+
+            if (turn.equals(LEAVER)) {
+                removeClient(turnClient);
+                break;
+            }
+
+            needSwitch = false;
+            String response = getShotResponse(turnClient, turn);
+
+            if (response.equals(MISS)) {
+                needSwitch = true;
+            } else if (response.equals(WINNER)) {
+
+                turnClient.sendMessage(WINNER);
+                switchClient(turnClient).sendMessage(OPPONENT_IS_THINKING + " " + turn);
+                switchClient(turnClient).sendMessage(LOSER);
+
+                removeAllClients();
+                break;
+            }
+
+            turnClient.sendMessage(response);
+            if (switchClient(turnClient) != null) {
+                switchClient(turnClient).sendMessage(OPPONENT_IS_THINKING + " " + turn);
+            }
+
+            if (needSwitch) {
+                turnClient = switchClient(turnClient);
+            }
+        }
+
+        if (playerOne != null) {
+            playerOne.sendMessage(OPPONENT_LEFT);
+        }
+    }
+
+    private void removeAllClients() {
+        while (playerOne != null) {
+            removeClient(playerOne);
+        }
+    }
+
+    public void removeClient(ClientHandler client) {
+        if (playerOne == null) return;
+        if (playerOne.equals(client)) {
+            playerOne = playerTwo;
+        }
+        playerTwo = null;
+    }
+
+    private void addPlayer(ClientHandler client) {
+        if (playerOne == null) {
+            playerOne = client;
+        } else {
+            playerTwo = client;
+        }
+    }
+
+    private boolean playersOnline() {
+        return playerTwo != null;
+    }
+
+    private boolean canGameStart() {
+        return playerTwo != null;
+    }
+
+    private void initializeField(String message, ClientHandler client) {
+
+        Map<PointsImpl, Boolean> field = new HashMap<>();
+
+        String[] positions = message.split("#");
+
+        logger.debug("String init field: " + message);
+
+        int x0, x1, y0, y1;
+
+        for (int i = 0; i < positions.length; ++i) {
+
+            String[] coordinates = positions[i].split(" ");
+            x0 = Integer.parseInt(coordinates[0]);
+            y0 = Integer.parseInt(coordinates[1]);
+            x1 = Integer.parseInt(coordinates[2]);
+            y1 = Integer.parseInt(coordinates[3]);
+
+            logger.debug("coordinates");
+            logger.debug(x0 + " " + y0 + " " + x1 + " " + y1);
+
+
+            field.put(new PointsImpl(x0, y0), true);
+
+            if (x0 == x1) {
+                for (int j = y0 + 1; j <= y1; ++j) {
+                    field.put(new PointsImpl(x0, j), true);
+                }
+            } else {
+                for (int j = x0 + 1; j <= x1; ++j) {
+                    field.put(new PointsImpl(j, y0), true);
+                }
+            }
+        }
+
+        if (client.equals(playerOne)) {
+            firstPlayerField = field;
+        } else {
+            secondPlayerField = field;
+        }
+    }
+
+    private ClientHandler switchClient(ClientHandler client) {
+        if (client.equals(playerOne)) {
+            return playerTwo;
+        }
+        return playerOne;
+    }
+
+    private String getShotResponse(ClientHandler client, String turn) {
+
+        String[] coordinates = turn.split(" ");
+
+        PointsImpl point = new PointsImpl(Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1]));
+
+        Map<PointsImpl, Boolean> field;
+        if (client.equals(playerOne)) {
+            field = secondPlayerField;
+        } else {
+            field = firstPlayerField;
+        }
+
+
+        if (field.containsKey(point)) {
+
+            field.put(point, false);
+
+            int nextX, nextY, dx, dy;
+
+            List<PointsImpl> directions = new ArrayList<>(
+                    Arrays.asList(
+                            new PointsImpl(0, 1),
+                            new PointsImpl(0, -1),
+                            new PointsImpl(1, 0),
+                            new PointsImpl(-1, 0)
+                    )
+            );
+
+
+            for (int i = 0; i < directions.size(); ++i) {
+
+                nextX = point.getX();
+                nextY = point.getY();
+
+                dx = directions.get(i).getX();
+                dy = directions.get(i).getY();
+
+                while (field.containsKey(new PointsImpl(nextX, nextY))) {
+                    if (field.get(new PointsImpl(nextX, nextY))) {
+                        return GOTHIM;
+                    }
+                    nextX += dx;
+                    nextY += dy;
+                }
+            }
+
+            if (field.containsValue(true)) {
+                return DEADNOW;
+            }
+
+            return WINNER;
+        }
+        return MISS;
+    }
+}
