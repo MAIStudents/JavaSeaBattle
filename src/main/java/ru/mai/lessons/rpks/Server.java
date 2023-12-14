0ee28697fec49e965d55e81c78916dfa3c1d43c5
Горщенко Денис Витальевич <65415967+DenisAmell@users.noreply.github.com>
2023-12-14 04:16:15 +0300

    update Server

--- src/main/java/ru/mai/lessons/rpks/Server.java
+++ src/main/java/ru/mai/lessons/rpks/Server.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Server {
 
@@ -15,6 +17,9 @@ public class Server {
     private Integer port = 8843;
     private ClientHandler playerOne;
     private ClientHandler playerTwo;
+
+    private Map<InitPoints, Boolean> firstPlayerField;
+    private Map<InitPoints, Boolean> secondPlayerField;
     public Server() {
     }
 
@@ -29,22 +34,18 @@ public class Server {
         try {
             serverSocket = new ServerSocket(port);
             logger.info("Сервер стартовал и ожидает подключение клиента");
-            while (true) {
+            while (playerTwo == null) {
                 Socket client = serverSocket.accept();
                 logger.info("Подключился новый клиент: " + client.toString());
-//                Scanner inputStream = new Scanner(client.getInputStream());
-//                while (inputStream.hasNext()) {
-//                    String text = inputStream.nextLine();
-//                    logger.info("Сообщение от клиента: " + text);
-//                    PrintWriter outputStream = new PrintWriter(client.getOutputStream());
-//                    outputStream.println("Эхо: " + text);
-//                    outputStream.flush();
-//                }
-//                inputStream.close();
                 ClientHandler clientHandler = new ClientHandler(client, this);
-                clients.add(clientHandler);
+                addPlayer(clientHandler);
                 new Thread(clientHandler).start();
             }
+
+            logger.info("Игра началась");
+            game();
+            logger.info("Конец игры");
+
         } catch (IOException e) {
             logger.error("Проблема с сервером", e);
         } finally {
@@ -58,9 +59,60 @@ public class Server {
         }
     }
 
-    public void sendMessageToChat(String message, String name) {
-        for (ClientHandler client : clients) {
-            client.sendMessage(name + ": " + message);
+    private void game() {
+
+    }
+
+    private void initializeField(String message, ClientHandler client) {
+
+        Map<InitPoints, Boolean> field = new HashMap<>();
+
+        String[] positions = message.split("#");
+
+        logger.debug("String init field: " + message);
+
+        int x0, x1, y0, y1;
+
+        for (int i = 0; i < positions.length; ++i) {
+
+            String[] coordinates = positions[i].split(" ");
+            x0 = Integer.parseInt(coordinates[0]);
+            y0 = Integer.parseInt(coordinates[1]);
+            x1 = Integer.parseInt(coordinates[2]);
+            y1 = Integer.parseInt(coordinates[3]);
+
+            logger.debug("coordinates");
+            logger.debug(x0 + " " + y0 + " " + x1 + " " + y1);
+
+
+            field.put(new InitPoints(x0, y0), true);
+
+            if (x0 == x1) {
+                for (int j = y0 + 1; j <= y1; ++j) {
+                    field.put(new InitPoints(x0, j), true);
+                }
+            } else {
+                for (int j = x0 + 1; j <= x1; ++j) {
+                    field.put(new InitPoints(j, y0), true);
+                }
+            }
+        }
+
+        if (client.equals(playerOne)) {
+            firstPlayerField = field;
+        } else {
+            secondPlayerField = field;
+        }
+    }
+
+    private boolean playersOnline() {
+        return playerTwo != null;
+    }
+    private void addPlayer(ClientHandler player) {
+        if(player == null) {
+            playerOne = player;
+        } else {
+            playerTwo = player;
         }
     }
 }
